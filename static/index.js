7236582e22842245b28bb7ad6c982905a65965f2
Monty Anderson <monty.linux@gmail.com>
2016-10-17 19:46:44 +0100

    display log

--- static/index.js
+++ static/index.js
@@ -2,6 +2,11 @@
 	const getJSON = url => fetch(url)
 		.then(response => response.json());
 
-	getJSON("/api/log")
-		.then(a => console.log(a));
+	getJSON("/api/log").then(log => {
+		const entries = Object.entries(log);
+
+		$(".log").html(entries.map(e => {
+			return `<span><b>${new Date(+e[0])}</b> ${e[1]}</span>`
+		}).join(""))
+	});
 })();

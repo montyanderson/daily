dc597ce6bb02b42ac1ef84063f06a5fd0424245b
Monty Anderson <monty.linux@gmail.com>
2016-11-08 23:59:00 +0000

    kinda works

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-const Trench = require("../trench");
-const Redis = require("ioredis");
+const crypto = require("crypto");
+const Trench = require("trench");
+const Redis = require("p-redis");
+const bluebird = require("bluebird");
+const moment = require("moment");
+
+const randomBytes = bluebird.promisify(crypto.randomBytes);
 
 const app = new Trench();
-const redis = new Redis();
-
-const log = "log";
+const db = Redis.createClient();
 
 app.use(Trench.static(__dirname + "/static"));
+app.use(Trench.bodyParser());
 
-app.get("/api/log", (req, res) => {
-	return redis.zrevrangebyscore(log, "+inf", "-inf", "WITHSCORES", "LIMIT", 0, 10)
-		.then(response => {
-			const obj = {};
+const key = "daily:logs";
 
-			for(let i = 0; i < response.length; i += 2) {
-				obj[response[i + 1]] = response[i];
-			}
+app.use((req, res) => {
+	res.redirect = function(path) {
+		this.writeHead(302, { "Location": path });
+		this.end();
+	};
+});
 
-			res.end(JSON.stringify(obj));
-		});
-}, (req, res) => {
-	res.end();
+app.get("/logs", (req, res) => {
+	const dates = [ +moment().subtract("1", "week"), +moment() ];
+
+	return db.zrangebyscoreAsync(key, ...dates)
+	.then(data => {
+		res.end(JSON.stringify(data.map(JSON.parse)));
+	});
+});
+
+app.post("/new", (req, res) => {
+	const { text } = req.body;
+
+	return randomBytes(256)
+	.then(data => {
+		const hash = crypto.createHash("sha256");
+		hash.update(data);
+
+		return hash.digest("base64");
+	})
+	.then(id => {
+		const date = +moment();
+		const member = JSON.stringify({ id, date, text });
+		return db.zadd(key, date, member);
+	})
+	.then(() => {
+		res.redirect("/");
+	});
 });
 
 app.listen(8080);
--- static/index.js
+++ static/index.js
@@ -1,12 +1,31 @@
-(() => {
-	const getJSON = url => fetch(url)
-		.then(response => response.json());
+function get(url, parseJSON, cb) {
+	const xhr = new XMLHttpRequest();
 
-	getJSON("/api/log").then(log => {
-		const entries = Object.entries(log);
-
-		$(".log").html(entries.map(e => {
-			return `<span><b>${new Date(+e[0])}</b> ${e[1]}</span>`
-		}).join(""))
+	xhr.addEventListener("load", function() {
+		try {
+			if(parseJSON) {
+				cb(null, JSON.parse(this.responseText));
+			} else {
+				cb(null, this.responseText);
+			}
+		} catch(error) {
+			cb(error);
+		}
 	});
-})();
+
+	xhr.open("GET", url);
+	xhr.send();
+};
+
+const logs = document.getElementById("logs");
+
+get("/logs", true, (err, data) => {
+	console.log(err || data);
+
+	console.log(data);
+	console.log(data.map);
+
+	logs.innerHTML = data.map(log => `
+	<strong>${log.date}</strong> ${log.text}
+	`);
+});
